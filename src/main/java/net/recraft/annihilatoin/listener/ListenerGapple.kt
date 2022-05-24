d1d3922a598f7f639cbf62fdf84e6558350a270e
h1yashi00 <hayamana3522@gmail.com>
2022-05-24 21:46:22 +0900

    ガップルエフェクト追加

--- src/main/java/net/recraft/annihilatoin/listener/ListenerGapple.kt
+++ src/main/java/net/recraft/annihilatoin/listener/ListenerGapple.kt
@@ -1,11 +1,15 @@
 package net.recraft.annihilatoin.listener
 
+import net.recraft.annihilatoin.objects.Game
+import org.bukkit.Bukkit
+import org.bukkit.Effect
 import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerItemConsumeEvent
 import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
+import org.bukkit.scheduler.BukkitRunnable
 
 class ListenerGapple: Listener {
     @EventHandler
@@ -15,5 +19,16 @@ class ListenerGapple: Listener {
         val player = event.player
         player.addPotionEffect(PotionEffect(PotionEffectType.SLOW_DIGGING, 20 * 5, 3))
         player.addPotionEffect(PotionEffect(PotionEffectType.WEAKNESS, 20 * 10, 3))
+        val effect = object: BukkitRunnable() {
+            override fun run() {
+                player.world.playEffect(player.location.apply{y+=2.5}, Effect.HEART,1)
+            }
+        }.runTaskTimer(Game.plugin, 0,5)
+
+        object: BukkitRunnable() {
+            override fun run() {
+                effect.cancel()
+            }
+        }.runTaskLater(Game.plugin, 20*30)
     }
 }
