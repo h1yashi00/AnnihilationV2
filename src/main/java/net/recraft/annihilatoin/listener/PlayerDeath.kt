d65dac548d6b756bee918c77f434c86bda499d1d
h1yashi00 <hayamana3522@gmail.com>
2022-05-28 04:00:08 +0900

    autoRespawnをしたときにアイテムを落とさないバグを解消｡

--- src/main/java/net/recraft/annihilatoin/listener/PlayerDeath.kt
+++ src/main/java/net/recraft/annihilatoin/listener/PlayerDeath.kt
@@ -1,12 +1,19 @@
 package net.recraft.annihilatoin.listener
 
+import net.recraft.annihilatoin.objects.Game
+
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.scheduler.BukkitRunnable
 
 class PlayerDeath: Listener {
     @EventHandler
     fun onPlayerDeath(event: PlayerDeathEvent) {
-        event.entity.spigot().respawn()
+        object:BukkitRunnable() {
+            override fun run() {
+                event.entity.spigot().respawn()
+            }
+        }.runTaskLater(Game.plugin, 1)
     }
 }
--- src/main/java/net/recraft/annihilatoin/listener/PlayerRespawn.kt
+++ src/main/java/net/recraft/annihilatoin/listener/PlayerRespawn.kt
@@ -14,26 +14,23 @@ import org.bukkit.scheduler.BukkitRunnable
 class PlayerRespawn: Listener {
     @EventHandler
     fun onRespawn(event: PlayerRespawnEvent) {
-        delaySpawn(event.player)
-    }
-    private fun delaySpawn(player: Player) {
-        val delayTask = object: BukkitRunnable() {
+        val player = event.player
+        if (!Game.isStart) {
+            player.realTeleport(Game.lobby.spawnLocation)
+            return
+        }
+        // kit 取得
+        val kitType = Game.getPlayerData(player.uniqueId).kitType // default CIVILIAN
+        val kit = KitGenerator.get(kitType)
+        // プレイヤーにkit装備を渡す
+        val team   = Game.getPlayerData(player.uniqueId).team
+        team?.let {kit.equip(player.inventory, it.color)}
+        val loc = team?.objects?.randomSpawn ?: Game.lobby.spawnLocation
+
+        object: BukkitRunnable() {
             override fun run() {
-                Bukkit.broadcastMessage("aaaaaa")
-                if (!Game.isStart) {
-                    player.realTeleport(Game.lobby.spawnLocation)
-                    return
-                }
-                // kit 取得
-                val kitType = Game.getPlayerData(player.uniqueId).kitType // default CIVILIAN
-                val kit = KitGenerator.get(kitType)
-                // プレイヤーにkit装備を渡す
-                val team   = Game.getPlayerData(player.uniqueId).team
-                team?.let {kit.equip(player.inventory, it.color)}
-                val loc = team?.objects?.randomSpawn ?: Game.lobby.spawnLocation
                 player.realTeleport(loc)
             }
-        }
-        Bukkit.getScheduler().scheduleSyncDelayedTask(Game.plugin, delayTask, 1) // バグ?でoneTick送らせないとrespawnできない
+        }.runTaskLater(Game.plugin,1)
     }
 }
