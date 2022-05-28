9eac1f792fcc7f1b13fc718e6dc1f1b4179bf9e8
h1yashi00 <hayamana3522@gmail.com>
2022-05-29 00:53:34 +0900

    PlayerInteractEventの代わりにBlockPlaceEventを使うように訂正した｡

--- src/main/java/net/recraft/annihilatoin/listener/PlayerPlaceBlock.kt
+++ src/main/java/net/recraft/annihilatoin/listener/PlayerPlaceBlock.kt
@@ -4,21 +4,19 @@ import net.recraft.annihilatoin.objects.ResourceBlocks
 import org.bukkit.ChatColor
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.block.Action
-import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.event.block.BlockPlaceEvent
 
 class PlayerPlaceBlock: Listener {
     @EventHandler
-    fun onPlaceBlock(event: PlayerInteractEvent) {
+    fun onPlaceBlock(event: BlockPlaceEvent) {
         if (event.player.isOp) return
-        val holdingItem = if (event.action != Action.RIGHT_CLICK_BLOCK) { return } else { event.player.itemInHand ?: return }
+        val holdingItem = event.block
         if (!ResourceBlocks.isResourceBlocks(holdingItem.type)) return
         event.isCancelled = true
     }
     @EventHandler
-    fun onPlaceBlock2(event: PlayerInteractEvent) {
-        if (event.action != Action.RIGHT_CLICK_BLOCK) return
-        if (128 < event.clickedBlock.location.y) {
+    fun onPlaceBlock2(event: BlockPlaceEvent) {
+        if (128 < event.block.location.y) {
             event.player.sendMessage("${ChatColor.RED}高すぎます!!128<=y以上はブロックを置くことができません")
             event.isCancelled = true
         }
