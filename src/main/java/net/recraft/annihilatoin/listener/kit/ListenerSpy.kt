ad7fbe9a9ff5ea5e23f54f24a01cdc724a9417ec
h1yashi00 <hayamana3522@gmail.com>
2022-05-30 11:47:52 +0900

    クリックで解除されるSpyの機能を追加した｡

--- src/main/java/net/recraft/annihilatoin/listener/kit/ListenerSpy.kt
+++ src/main/java/net/recraft/annihilatoin/listener/kit/ListenerSpy.kt
@@ -6,8 +6,11 @@ import net.recraft.annihilatoin.objects.kit.KitType
 import net.recraft.annihilatoin.show
 import org.bukkit.Bukkit
 import org.bukkit.ChatColor
+import org.bukkit.Location
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerToggleSneakEvent
 import org.bukkit.scheduler.BukkitRunnable
@@ -20,6 +23,40 @@ class ListenerSpy: Listener {
         val player = event.player
         invisiblePlayers.forEach { val invPlayer = Bukkit.getPlayer(it); player.hidePlayer(invPlayer) }
     }
+    @EventHandler
+    fun onInteract(event: PlayerInteractEvent) {
+        val player = event.player
+        val hitPlayer = threeBlockRangePlayer(player)?: return
+        if (!invisiblePlayers.contains(hitPlayer.uniqueId)) return
+        hitPlayer.sendMessage("You found by ${player.name}")
+        hitPlayer.show()
+        invisiblePlayers.remove(hitPlayer.uniqueId)
+    }
+    private fun threeBlockRangePlayer(player: Player): Player? {
+        val loc = player.location.clone()
+        val dir = loc.direction.clone()
+        for (i in 0..3) {
+            dir.multiply(1)
+            val newLoc = loc.add(dir)
+            return getPlayerByLocation(player, newLoc, 1.5F) ?: continue
+        }
+        return null
+    }
+
+    private fun getPlayerByLocation(excludePlayer: Player, loc: Location, r: Float): Player? {
+        loc.world.entities.forEach {
+            if (it.location.distanceSquared(loc) < r) {
+                if (it is Player) {
+                    if (it.player.uniqueId == excludePlayer.uniqueId) {
+                        return@forEach
+                    }
+                    return it.player
+                }
+            }
+        }
+        return null
+    }
+
     @EventHandler
     fun onSneak(event: PlayerToggleSneakEvent) {
         val player = event.player
