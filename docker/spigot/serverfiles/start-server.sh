363a991ed153f3e1c89fa61a5b8a338e04624d46
h1yashi00 <hayamana3522@gmail.com>
2022-06-12 16:47:06 +0900

    仮想環境を構築した｡docker-component build docker-component upで起動できる｡

---
+++ docker/spigot/serverfiles/start-server.sh
@@ -0,0 +1,13 @@
+set -e
+
+cp -Rf /spigot-files/* /spigot/
+mkdir -p /spigot/plugins
+
+java -Xmx4g -Xms256m -jar /spigot/spigot*.jar nogui
+
+echo "eula=true" >> eula.txt
+
+# ディレクトリを移動しないと､/spigot/plugins/が読み込まれない
+cd /spigot
+
+java -Xmx4g -Xms256m -jar spigot*.jar nogui
