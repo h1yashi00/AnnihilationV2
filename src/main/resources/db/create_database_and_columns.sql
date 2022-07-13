2b6f0d34d1c78bdaff070689328c6891263877e6
h1yashi00 <hayamana3522@gmail.com>
2022-07-14 01:41:04 +0900

    ゲームの履歴を保存するデータ・ベースのテーブル(仮)

--- src/main/resources/db/create_database_and_columns.sql
+++ src/main/resources/db/create_database_and_columns.sql
@@ -47,3 +47,45 @@ create table if not exists playerdata (
     primary key (player_uuid),
     foreign key (rank_id) references staff_rank(rank_id)
 );
+
+--
+--create table if not exists player_kill_history (
+--    game_history_id smallint not null
+--    killed_time timestamp not null
+--    killer UUID not null
+--    victim UUID not null
+--    foreign key (game_history_id) references game_history(game_history_id)
+--    foreign key (killer) references playerdata(player_uuid)
+--    foreign key (victim) references playerdata(player_uuid)
+--)
+--
+--create table if not exists player_nexus_damaged (
+--    game_history_id smallint not null
+--    killed_time timestamp not null
+--    damage smallint not null
+--    damager UUID not null
+--    victim_team UUID not null
+--)
+--
+--create table if not exists boss_kill_hisotry (
+--    game_history_id smallint not null
+--    killed_time timestamp not null
+--    foreign key (game_history_id) references game_history(game_history_id)
+--)
+--
+--create table if not exists teamColor (
+--)
+--
+--create table if not exists team (
+--    game_history_id smallint not null,
+--    color_id        smallint not null,
+--)
+--
+--create table if not exists game_history (
+--     game_history_id smallint NOT NULL AUTO_INCREMENT,
+--     start_time      timestamp not null default current_timestamp,
+--     finish_time     timestamp not null default current_timestamp,
+--     map             map_id    not null,
+--     booster         booster_id not null,
+--     win smallint not null;
+--)
